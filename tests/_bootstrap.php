d98005895679c2952156ed5f0226b98258eae4e1
javier gomez <javier.gomez.corio@gmail.com>
2014-10-18 11:05:39 +0200

    fix bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,3 +2,5 @@
 // This is global bootstrap for autoloading
 
 \Codeception\Util\Autoload::registerSuffix('Group', __DIR__ . DIRECTORY_SEPARATOR . '_groups');
+\Codeception\Util\Autoload::register('Joomla3', 'Page', __DIR__ . DIRECTORY_SEPARATOR . '_pages' . DIRECTORY_SEPARATOR . 'joomla3');
+\Codeception\Util\Autoload::register('Joomla3\installation\installation', 'Page', __DIR__. '/_pages/joomla3/installation');
